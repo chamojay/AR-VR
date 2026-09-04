acedd0e231a67f5a9e0b0b6a32c3c3953ee03d06
ChamodJay-pickme <chamod.jayaweera@pickme.lk>
2026-09-04 15:21:40 +0530

    Configure GitHub Actions deployment and relative paths on develop

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  base: '/AR-VR/',
+  base: './',
   root: '.',
   publicDir: 'public',
   plugins: [basicSsl()],
